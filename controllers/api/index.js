0238a7ff7000871638c6e6e631aa4e7e5577a1a1
Gabrielle Belanger <162386528+gcbel@users.noreply.github.com>
2024-07-10 17:52:22 -0400

    feat: add get route for all posts

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,10 +2,8 @@
 const router = require('express').Router();
 
 /* ROUTES */
-const postRoutes = require('./posts.js');
 const userRoutes = require('./users');
 
-router.use('/posts', postRoutes);
 router.use('/users', userRoutes);
 
 /* EXPORTS */
--- controllers/home.js
+++ controllers/home.js
@@ -1,9 +1,26 @@
 /* DEPENDECIES */
 const router = require('express').Router();
-const withAuth = require('../utils/auth.js')  // Import authentication middleware
-const { } = require('../models');             // Import models
+const authorized = require('../utils/auth.js')  // Import authentication middleware
+const { Post, User } = require('../models');    // Import models
 
 /* ROUTES */
+/* Get request for homepage, gets all posts */
+router.get('/', authorized, async (req, res) => {
+    try {
+        // Get all posts
+        const posts = await Post.findAll({
+            include: [{ model: User }]
+        });
+
+        // Map data
+
+        // Render posts
+        res.render('homepage', {
+            posts,
+            loggedIn: req.session.loggedIn,
+          });
+    } catch (err) {res.status(500).json(err)};
+});
 
 /* EXPORTS */
 module.exports = router;
