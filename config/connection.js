6a9cc458fa3a5690a0eb1c1ba84a039e228b5b21
Gabrielle Belanger <162386528+gcbel@users.noreply.github.com>
2024-07-11 11:21:15 -0400

    feat: add database prefix for render deployment

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,23 @@
 /* DEPENDENCIES */
-const Sequelize = require('sequelize');
-require('dotenv').config();
+const Sequelize = require("sequelize");
+require("dotenv").config();
 
 /* SEQUELIZE */
 const sequelize = process.env.DB_URL
-  ? new Sequelize(process.env.DB_URL)
+  ? new Sequelize(process.env.DB_URL, {
+      hooks: {
+        beforeDefine: function (columns, model) {
+          model.tableName = `${process.end.DB_NAME}_${model.name.singluar}`;
+        },
+      },
+    })
   : new Sequelize(
       process.env.DB_NAME,
       process.env.DB_USER,
       process.env.DB_PASSWORD,
       {
-        host: 'localhost',
-        dialect: 'postgres',
+        host: "localhost",
+        dialect: "postgres",
       }
     );
 
