5299cd86013bfb1f453368be1718b51598074138
Gabrielle Belanger <162386528+gcbel@users.noreply.github.com>
2024-07-20 16:49:00 -0400

    feat: functional homepage

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,23 +1,52 @@
 /* DEPENDECIES */
 const router = require("express").Router();
 const { authenticate } = require("../utils/helpers.js"); // Import authentication middleware
-const { Post, User } = require("../models/index.js"); // Import models
+const { Post, User, Comment } = require("../models/index.js"); // Import models
 
 /* ROUTES */
-/* Get request for homepage, gets all posts */
-router.get("/", authenticate, async (req, res) => {
+/* Get request for homepage */
+router.get("/", async (req, res) => {
   try {
-    // Get all posts
-    const posts = await Post.findAll({
-      include: [{ model: User }],
-    });
-
-    // Map data
-
-    // Render posts
-    res.render("homepage", {
-      posts,
+    // Render page
+    res.render("home", {
       loggedIn: req.session.loggedIn,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get("/posts", async (req, res) => {
+  try {
+    // Get posts
+    postData = await Post.findAll({
+      include: [{ model: User, model: Comment }],
+    });
+    const posts = postData.map((posts) => posts.get({ plain: true }));
+
+    // Render page
+    res.render("posts", {
+      loggedIn: req.session.loggedIn,
+      posts,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get("/dashboard/username", async (req, res) => {
+  try {
+    // Get posts
+    postData = await Post.findAll({
+      where: { username: req.params.name },
+      include: [{ model: User, model: Comment }],
+    });
+    const posts = postData.map((posts) => posts.get({ plain: true }));
+
+    // Render page
+    res.render("dashboard", {
+      loggedIn: req.session.loggedIn,
+      userPosts,
     });
   } catch (err) {
     res.status(500).json(err);
