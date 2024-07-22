3d81ee788020d59b6f1f1a1edf719b279c5a9877
Gabrielle Belanger <162386528+gcbel@users.noreply.github.com>
2024-07-22 17:33:52 -0400

    add color changing icon:

---
+++ public/js/script.js
@@ -0,0 +1,18 @@
+/* VARIABLES */
+const icon = document.getElementById("browser-icon");
+const darkMode = window.matchMedia("(prefers-color-scheme: dark)");
+
+/* FUNCTIONS */
+function updateIcon(darkMode) {
+  console.log(darkMode);
+  if (darkMode.matches) {
+    icon.href = "images/logo-dark.png";
+  } else {
+    icon.href = "images/logo.png";
+  }
+}
+
+/* INITIALIZER */
+document.addEventListener("DOMContentLoaded", () => {
+  updateIcon(darkMode);
+});
