abb7be745a7dd43d7f7ab85384da4984ff92db8a
Gabrielle Belanger <162386528+gcbel@users.noreply.github.com>
2024-07-27 22:36:11 -0400

    refactor: look of google oauth sign in

--- controllers/api/content-routes.js
+++ controllers/api/content-routes.js
@@ -2,27 +2,31 @@
 const router = require("express").Router();
 const { User, Post, Comment } = require("../../models");
 
-/* Post route to /api/content/newpost, creates a new post */
-router.post("/signup", async (req, res) => {
+/* Post route to /api/content/post, creates a new post */
+router.post("/post", async (req, res) => {
   try {
-    // Create credentials
-    const user = await User.create({
-      first: req.body.first,
-      last: req.body.last,
-      username: req.body.user,
-      email: req.body.email,
-      password: req.body.password,
+    // Get user
+    const user = await User.findOne({
+      where: { username: req.session.username },
+    });
+    if (!user) {
+      return res.status(404).json({ message: "User not found." });
+    }
+
+    // Create a new post
+    const post = await Post.create({
+      title: req.body.title,
+      content: req.body.content,
+      date: new Date(),
+      user_id: user.id,
     });
 
-    // Sign in user
-    req.session.save(() => {
-      req.session.signedIn = true;
-      req.session.username = req.body.user;
-      res.status(200).json(user);
-    });
+    res.status(200).json(post);
   } catch (err) {
     console.error(err);
-    res.status(500).json(err);
+    res
+      .status(500)
+      .json({ message: "Failed to create a new post.", error: err });
   }
 });
 
--- public/js/new-post.js
+++ public/js/new-post.js
@@ -1,2 +1,53 @@
 /* VARIABLES */
 const newPostButton = document.querySelector("#new-post");
+const xPostButton = document.querySelector("#x-out");
+const newPostSection = document.querySelector("#new-post-section");
+const postNewPost = document.querySelector("#post-new-post-button");
+const postTitleAlert = document.querySelector("#post-title-alert");
+const postContentAlert = document.querySelector("#post-content-alert");
+const postAlert = document.querySelector("#post-alert");
+
+/* FUNCTIONS */
+/* Show/hide new post prompts */
+function toggleNewPostSection() {
+  newPostSection.classList.toggle("hidden");
+  newPostSection.classList.toggle("center");
+}
+
+/* Post new post */
+async function submitNewPost() {
+  const title = document.querySelector("#post-title").value;
+  const content = document.querySelector("#post-content").value;
+
+  if (!title) {
+    postTitleAlert.classList.remove("hidden");
+  } else {
+    postTitleAlert.classList.add("hidden");
+  }
+
+  if (!content) {
+    postContentAlert.classList.remove("hidden");
+  } else {
+    postContentAlert.classList.add("hidden");
+  }
+
+  if (title && content) {
+    const response = await fetch("/api/content/post", {
+      method: "POST",
+      body: JSON.stringify({ title, content }),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    if (response.ok) {
+      toggleNewPostSection();
+      location.reload();
+    } else {
+      postAlert.classList.remove("hidden");
+    }
+  }
+}
+
+/* EVENT LISTENERS */
+newPostButton.addEventListener("click", toggleNewPostSection);
+xPostButton.addEventListener("click", toggleNewPostSection);
+postNewPost.addEventListener("click", submitNewPost);
